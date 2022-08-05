f403197c8a5f5c6ffb231bb2684077fcb3ae8b83
amiminn <kochengpoi@gmail.com>
2022-08-05 13:29:46 +0700

    starter

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <IndexView />
+  </div>
+</template>
+<script>
+import IndexView from "./views/IndexView.vue";
+export default {
+  name: "App",
+  components: {
+    IndexView,
+  },
+};
+</script>
---
+++ src/views/IndexView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="nav-fixed">
+    <NavbarHeader />
+    <SideNav />
+  </div>
+</template>
+<script>
+import NavbarHeader from "./components/NavbarHeader.vue";
+import SideNav from "./components/SideNav.vue";
+export default {
+  name: "IndexView",
+  data() {
+    return {};
+  },
+  components: {
+    NavbarHeader,
+    SideNav,
+  },
+};
+</script>
